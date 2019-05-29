b9823bce9ea6fd56e38eafc0ba0401c06d18f1fa
3a7132350 <48341794+3a7132350@users.noreply.github.com>
2019-05-29 14:20:11 +0800

    Add files via upload

---
+++ 23.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){	
+		FILE *fptr;
+		char C;
+		fptr=fopen("data.txt","r");
+		if(fptr!=NULL){
+			while((C=getc(fptr))!=EOF){  
+				printf("%c", C);
+			}
+			fclose(fptr);
+		}
+		else{
+			printf("AER¡Ñ?}¡ÓO¢D¢F¡ÓN");
+		}
+		system("pause");
+		return 0;
+} 
+
